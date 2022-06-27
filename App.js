4cabff714d62dd00065aabc2232da6fad1f2c6f6
Brandy Odhiambo <71652231+brandy-atieno@users.noreply.github.com>
2022-06-27 15:35:52 +0300

    Reasiagned one route

--- App.js
+++ App.js
@@ -7,9 +7,7 @@ server.use(bodyparser.urlencoded({extended: true}));
 const {router}=require('./routes/users')
 
 server.use('/', router)
-server.use('/users', router)
-server.use('/user/:email', router)
-server.use('/login', router)
+
 
 
 
